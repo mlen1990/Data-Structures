a58f2c2f9488dfd3fcfedf9186c16e05339e949a
Martin Len <mlen1990@Martins-MacBook-Air-2.local>
2013-07-29 00:17:33 -0700

    Added an algorithm to compute the nth Fibonacci number iteratively.

--- IterativeFibonacci.java
+++ IterativeFibonacci.java
@@ -1,36 +1,43 @@
-/* RecursiveFibonacci.java */
+/* IterativeFibonacci.java */
 
-public class RecursiveFibonacci {
+public class IterativeFibonacci {
 
   /*
-   *  This function computes the nth Fibonacci number recursively.
+   *  This function computes the nth fibonacci number iteratively.
    *  @param n the nth fibonacci number to be computed.
    *  @return an integer representing the nth fibonacci number.
+   *  Running time: O(n)
+   *  Space Complexity: O(1)
    */
   public static int fibonacci(int n) {
     if (n < 0) {
-      System.err.println("The input must be positive.");
+      System.out.println("Input must be positive.");
       return -1;
-    } else if (n == 0) {
-      return 0;
     } else if (n == 1) {
       return 1;
-    } else {
-      return fibonacci(n - 1) + fibonacci(n - 2);
     }
+    int f1 = 0;
+    int f2 = 1;
+    int fn = 0;
+    for (int i = 2; i < n + 1; i++) {
+      fn = f1 + f2;
+      f1 = f2;
+      f2 = fn;
+    }
+    return fn;
   }
 
   /*
    *  To compile this program using the command line, type
-   *  "javac RecursiveFibonacci.java"
+   *  "javac IterativeFibonacci.java"
    *  To run this program using the command line, type
-   *  "java RecursiveFibonacci <n>"
+   *  "java IterativeFibonacci <n>"
    *  On a successful run, the nth fibonacci number is outputted
    *  to stdout.
    */
   public static void main(String[] args) {
     if (args.length != 1) {
-      System.out.println("Usage: java RecursiveFibonacci <n>");
+      System.out.println("Usage: java IterativeFibonacci <n>");
       return;
     }
     System.out.println(fibonacci(Integer.parseInt(args[0])));
